689d0ddf5cbcd968ea097285a54ef40910fc8ccc
sommaysingh0-coder <sommaysingh0@gmail.com>
2026-06-20 23:11:56 +0530

    Add files via upload

--- tips.py
+++ tips.py
@@ -194,10 +194,10 @@ if option == "Joke":
           import urllib.parse
           st.subheader("🌐 Open Web - Search Anything on Google")
           search_query = st.text_input("what i should search?:", placeholder="Type here...")
-          search_query
-          encoded_query = urllib.parse.quote_plus(search_query)
-          google_search_url = f"https://www.google.com/search?q={encoded_query}"
-          sa = st.link_button(f"Google Par '{search_query}' Search Karo 🚀", google_search_url) 
+          if search_query :
+             encoded_query = urllib.parse.quote_plus(search_query)
+             google_search_url = f"https://www.google.com/search?q={encoded_query}"
+             sa = st.link_button(f"Google Par '{search_query}' Search Karo 🚀", google_search_url) 
    
 
 # ABCD######################################
