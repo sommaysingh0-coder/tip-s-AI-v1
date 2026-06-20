18c5b86728e1e31a061322618f5a8153a3c00140
sommaysingh0-coder <sommaysingh0@gmail.com>
2026-06-20 22:24:38 +0530

    Add files via upload

--- tips.py
+++ tips.py
@@ -190,10 +190,10 @@ if option == "Joke":
            st.audio("joke_voice.mp3" , format="audio/mp3")
 #open web#################################
     if option == "open web":
-      st.write("i can search anything on Google. ")
-      import urllib.parse
-      st.subheader("🌐 Open Web - Search Anything on Google")
-      search_query = st.text_input("what i should search?:", placeholder="Type here...")
+        st.write("i can search anything on Google. ")
+        import urllib.parse
+        st.subheader("🌐 Open Web - Search Anything on Google")
+        search_query = st.text_input("what i should search?:", placeholder="Type here...")
 if search_query:
     encoded_query = urllib.parse.quote_plus(search_query)
     google_search_url = f"https://www.google.com/search?q={encoded_query}"
