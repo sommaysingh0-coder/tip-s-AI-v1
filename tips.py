5c17a4b22b1f57414926d467d427ef0c317863a7
sommaysingh0-coder <sommaysingh0@gmail.com>
2026-06-20 23:25:34 +0530

    Add files via upload

--- tips.py
+++ tips.py
@@ -197,7 +197,7 @@ if option == "Joke":
         if search_query:
             encoded_query = urllib.parse.quote_plus(search_query)
             google_search_url = f"https://www.google.com/search?q={encoded_query}"
-            st.link_button(f"Google Par '{search_query}' Search Karo 🚀", google_search_url) 
+            search_query = st.link_button(f"Google Par '{search_query}' Search Karo 🚀", google_search_url) 
    
 
 # ABCD######################################
